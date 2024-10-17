49b3a9835d6d36d398b9588b0676368b1f1d88f9
Septillihedron <resha3adnandaru@gmail.com>
2024-10-17 15:16:22 +0700

    Switch to gradle kotlin, add publication

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    java
+    `maven-publish`
+}
+
+group = "me.sepdron"
+version = "2.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    maven { url = uri("https://libraries.minecraft.net/")}
+    maven {
+        name = "spigotmc-repo"
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+    maven {
+        name = "sonatype"
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT")
+    compileOnly("com.mojang:authlib:3.18.38")
+}
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "xemorReleases"
+            url = uri("https://repo.xemor.zip/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                isAllowInsecureProtocol = true
+                create<BasicAuthentication>("basic")
+            }
+        }
+
+        maven {
+            name = "xemorSnapshots"
+            url = uri("https://repo.xemor.zip/snapshots")
+            credentials(PasswordCredentials::class)
+            authentication {
+                isAllowInsecureProtocol = true
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = rootProject.group.toString()
+            artifactId = rootProject.name
+            version = rootProject.version.toString()
+            from(project.components["java"])
+        }
+    }
+}
